f8e1c9cbb61d5367faae00fc9149102deb82bcd3
shakib-mia <smdshakibmia2001@gmail.com>
2025-11-27 19:53:47 +0600

    banner section done

--- app/components/Banner/Banner.js
+++ app/components/Banner/Banner.js
@@ -1,7 +1,50 @@
 import React from "react";
+import banner from "./../../assets/banner.jpg";
+import Button from "../Button/Button";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <section
+      className="bg-size-[110%] bg-center bg-no-repeat relative text-center text-white"
+      style={{ backgroundImage: `url(${banner.src})` }}
+    >
+      <div className="container">
+        <div className="lg:w-5/6 mx-auto">
+          <h2 className="uppercase my-7" data-animate="fade-in-down">
+            Lorem ipsum dolor sit amet consectetur. Quis adipiscing purus
+            egestas aliquam viverra mi.
+          </h2>
+
+          <div className="space-y-4 font-semibold lg:w-5/6 mx-auto">
+            <p data-animate="fade-in-up">
+              Lorem ipsum dolor sit amet consectetur. Mattis justo euismod
+              volutpat vestibulum nisi at ac risus amet. Mi accumsan sagittis
+              justo pellentesque id sed. Id tellus id luctus id. At quis nunc
+              libero urna arcu vulputate sed ut. Nisl porta massa diam
+              condimentum nulla quam.
+            </p>
+            <p data-animate="fade-in-up">
+              Lorem ipsum dolor sit amet consectetur. Volutpat in dictum nec
+              condimentum ultrices non. Ornare semper in tincidunt pellentesque
+              cras mauris in vitae. At viverra quis eu malesuada vel et
+              porttitor. Nulla luctus quam lacus lacus non at. Tincidunt morbi
+              feugiat a pulvinar euismod natoque nulla ligula. Tincidunt cursus
+              vitae leo.
+            </p>
+          </div>
+
+          <Button
+            className="bg-white! text-black! shadow-lg active:shadow-none border-none hover:bg-primary! hover:text-white! mt-7"
+            href="https://templatehearth.vercel.app/contact"
+            target="_blank"
+            data-animate="fade-in-up"
+          >
+            Get Started
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Bikes from "./components/Bikes/Bikes";
 import bg1 from "./assets/bg1.png";
 import About from "./components/About/About";
+import Banner from "./components/Banner/Banner";
 
 export default function Home() {
   return (
@@ -11,12 +12,14 @@ export default function Home() {
       <Navbar />
       <Header />
       <div
-        className="bg-cover bg-center lg:pb-36 pb-20 md:pb-28"
+        className="bg-cover bg-center lg:pb-36 pb-20 md:pb-28 bg-no-repeat"
         style={{ backgroundImage: `url(${bg1.src})` }}
       >
         <Bikes />
         <About />
       </div>
+
+      <Banner />
     </>
   );
 }
