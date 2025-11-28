07923aaa679f68c15b38168c21f4b863031ddf64
shakib-mia <smdshakibmia2001@gmail.com>
2025-11-28 19:26:34 +0600

    feat: simplify remotePatterns configuration in images setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    remotePatterns: [new URL("https://randomuser.me/**")],
   },
 };
 
